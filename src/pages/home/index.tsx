535e4e0465a802dea7fb8bb8eb845dd4df2898cf
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2022-09-26 00:45:43 +0500

    changes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
 import {FC} from 'react'
 import styles from "./index.module.css"; 
+import {Link} from "react-router-dom"; 
 
 const Home : FC = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.container}>
          <ul className={styles.navbar_links} >
-            <li className={styles.navbar_links_element} >Fake Drivers account</li>
+            <Link to="/drivers" className={styles.navbar_links_element} >Fake Drivers account</Link>
             <li className={styles.navbar_links_element} >Generate Dispatch</li>
             <li className={styles.navbar_links_element} >Fake Drivers account</li>
             <li className={styles.navbar_links_element} >Fake Drivers account</li>
